e2c769a43fdb98dc869051cc6d5858548a1fd242
Amlan Sahoo <amlansahoo005@gmail.com>
2025-06-30 14:05:46 +0530

    BB-31|amlan|kafka confluent integration

--- src/main/java/com/bite/buddy/kafka/KafkaProducerConfig.java
+++ src/main/java/com/bite/buddy/kafka/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.bite.buddy.kafka;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,11 +16,28 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaUrl;
+
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String saslConfig;
+
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String saslMech;
+
+    @Value("${spring.kafka.properties.security.protocol}")
+    private String secProtocol;
+
+
     @Bean
     public ProducerFactory<String, NotificationEvent> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put("sasl.jaas.config", saslConfig);
+        config.put("sasl.mechanism", saslMech);
+        config.put("security.protocol", secProtocol);
+
         // Do not set VALUE_SERIALIZER_CLASS_CONFIG in config map here
 
         // Explicitly create JsonSerializer and disable type headers
